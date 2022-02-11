4ca6224891ad0fd14bae625cac35b7a61d443a7a
David Hillton <davidhillton@gmail.com>
2022-02-11 02:30:09 -0300

    carga del proyecto final

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+ScrollReveal().reveal('.elementoLi' , {delay: 500, reset: true});
+
+function clikCompra(){
+
+  var toastLiveExample = document.getElementById('liveToast')
+
+  var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+}
